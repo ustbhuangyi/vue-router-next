425c2a84ff51f6d73a125f29f632c43eb170cdae
Eduardo San Martin Morote <posva13@gmail.com>
2020-09-09 17:22:21 +0200

    feat(devtools): add more

--- src/router.ts
+++ src/router.ts
@@ -1104,7 +1104,7 @@ export function createRouter(options: RouterOptions): Router {
         unmountApp.call(this, arguments)
       }
 
-      if (__DEV__) {
+      if (__DEV__ && __BROWSER__) {
         addDevtools(app, router, matcher)
       }
     },
